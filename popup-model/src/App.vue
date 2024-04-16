aa5cfff7b444394119bd83968fb66d1c35afbcf6
tushar <tushargadher123@gmail.com>
2024-04-16 16:07:12 +0530

    teleport component and created popup model

---
+++ popup-model/src/App.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="app-container">
+    <h1>Teleport Component Example</h1>
+    <p>
+      In Vue.js, creating a teleport component allows you to render a
+      component's HTML output at a different place in the DOM hierarchy than
+      where the component itself is located. This is particularly useful when
+      you want to render content in a different part of the DOM tree, like
+      directly under the body tag, outside of the current component's scope.
+    </p>
+    <button @click="showModel = true">Show Model</button>
+
+    <teleport to=".model-root">
+      <Model v-show="showModel" @closepopup="showModel = false">
+        <template v-slot:heading>
+          <h1>Basic Usage</h1>
+        </template>
+        <template v-slot:body>
+          <p>
+            Wrap the content you want to teleport with teleport and use the to
+            attribute to specify the target destination where the content should
+            be rendered.
+          </p>
+        </template>
+      </Model>
+    </teleport>
+  </div>
+</template>
+
+<script>
+import Model from "./components/popup-model.vue";
+
+export default {
+  name: "App",
+  components: {
+    Model,
+  },
+  data() {
+    return {
+      showModel: false,
+    };
+  },
+};
+</script>
+
+<style>
+* {
+  margin: 0;
+}
+.app-container {
+  margin: 20px;
+  border: 1px solid black;
+  height: 30vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-around;
+  padding: 10px;
+}
+</style>
---
+++ popup-model/src/components/popup-model.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="container">
+    <div class="model">
+      <slot name="heading"></slot>
+
+      <slot name="body"></slot>
+
+      <button @click="$emit('closepopup')">Close</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "popup-model",
+};
+</script>
+
+<style scoped>
+.container {
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  background-color: rgba(0, 0, 0, 0.3);
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-around;
+}
+.model {
+  background-color: white;
+  border: 1px solid black;
+  border-radius: 10px;
+  height: 50%;
+  width: 30%;
+  margin: 20px;
+  border: 1px solid black;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-around;
+  padding: 10px;
+}
+</style>
