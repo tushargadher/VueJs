69177cb430cb3ba1a975e50516e4e3e5992da7ed
tushar <tushargadher123@gmail.com>
2024-04-16 18:36:04 +0530

    composition api

---
+++ composition-api/src/components/HelloWorld.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h1>Composition API</h1>
+    <p>{{ message }}</p>
+  </div>
+</template>
+
+<script>
+import { ref } from "vue";
+export default {
+  name: "HelloWorld",
+  setup() {
+    const message = ref("Hello this is composition api example");
+    console.log(message);
+    return {
+      // key:pair
+      // since both are same we write only one
+      message,
+    };
+  },
+};
+</script>
+
+<style scoped></style>
