48adb9b8d73f7732971d1dcbc034445ede1ce9a5
tushar <tushargadher123@gmail.com>
2024-04-18 12:02:53 +0530

    provide and inject in composition api

--- provide-inject/src/App.vue
+++ provide-inject/src/App.vue
@@ -1,14 +1,29 @@
 <template>
   <p>UserName form App.vue {{ name }}</p>
+  <p>{{ productName }} {{ company }}</p>
   <FirstComponent />
 </template>
 
 <script>
+import { reactive, toRefs, provide } from "vue";
 import FirstComponent from "./components/FirstComponent.vue";
 export default {
   name: "App",
+
   components: {
     FirstComponent,
+  },
+  setup() {
+    let state = reactive({
+      productName: "SmartWatch",
+      company: "Boat",
+    });
+    //provide take two agrument first is key and second is value
+    provide("provide_name", "Vuejs ");
+    provide("productData", state);
+    return {
+      ...toRefs(state),
+    };
   },
   data() {
     return {
@@ -18,7 +33,7 @@ export default {
   // provide: {
   //   username: "Tushar Gadher",
   // },
-  
+
   provide() {
     return {
       username: this.name,
--- provide-inject/src/components/ThirdComponent.vue
+++ provide-inject/src/components/ThirdComponent.vue
@@ -3,11 +3,23 @@
     <h4>Third Component</h4>
     <p>{{ username }}</p>
     <p>{{ secondMessage }}</p>
+    <p>Name from App Component {{ name }}</p>
+    <p>{{ productName }}</p>
   </div>
 </template>
 <script>
+import { inject, toRefs } from "vue";
 export default {
   name: "ThirdComponent",
+  setup() {
+    //inject have two parameter first is key and second is default value
+    const name = inject("provide_name");
+    const productData = inject("productData", {productName:"Default Name"});
+    return {
+      name,
+      ...toRefs(productData),
+    };
+  },
   inject: ["username", "secondMessage"],
 };
 </script>
